e49430eb3c11a342e5621bc72724e9d85278f6ff
Torsten Raudssus <torsten@raudss.us>
2024-07-31 03:14:49 +0200

    More POD

--- lib/MooseX/JSONSchema/AttributeTrait.pm
+++ lib/MooseX/JSONSchema/AttributeTrait.pm
@@ -36,3 +36,35 @@ sub _build_json_schema_property_data {
 }
 
 1;
+
+
+=head1 SYNOPSIS
+
+  package OtherClass;
+
+  use Moose;
+  use MooseX::JSONSchema;
+
+  ...
+
+  has something => (
+    traits => [qw( MooseX::JSONSchema::AttributeTrait )],
+    json_schema_description => $description,
+    json_schema_type => 'string',
+    predicate => 'has_something',
+    is => 'ro',
+    isa => 'Str',
+  );
+
+=head1 SUPPORT
+
+Repository
+
+  https://github.com/Getty/perl-moosex-jsonschema
+  Pull request and additional contributors are welcome
+ 
+Issue Tracker
+
+  https://github.com/Getty/perl-moosex-jsonschema/issues
+
+=cut
